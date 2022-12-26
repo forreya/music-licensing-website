ebb4e6f2627342ef79b2d62e2988792d13535a32
Ryan Lai <ryanlai12@gmail.com>
2022-12-26 14:09:07 +0000

    Created 'ScrollToTop' function. Setup route to home page

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,29 @@ import {
   Routes,
   Route,
   useLocation,
-} from 'react-router-dom'
+} from 'react-router-dom';
+import Home from "./scenes/home/Home";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
 
 function App() {
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
