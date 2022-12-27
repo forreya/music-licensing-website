eeb95cf644b4c9a23bf6aa90a29912b0054477cc
Ryan Lai <ryanlai12@gmail.com>
2022-12-27 13:19:25 +0000

    Added icons & inner div for Navbar

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -10,25 +10,59 @@ import { useNavigate } from 'react-router-dom' ;
 import { shades } from '../../theme';
 
 const Navbar = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
 
-    return (
-        <Box
+  return (
+    <Box
+      display = "flex"
+      alignItems = "center"
+      width = "100%"
+      height = "60px"
+      backgroundColor = "rgba(255, 255, 255, 0.95)"
+      color = "black"
+      position = "fixed"
+      top = "0"
+      left = "0"
+      zIndex = "1"
+    >
+      <Box
+        width = "80%"
+        margin = "auto"
         display = "flex"
+        justifyContent = "space-between"
         alignItems = "center"
-        width = "100%"
-        height = "60px"
-        backgroundColor = "rgba(255, 255, 255, 0.95)"
-        color = "black"
-        position = "fixed"
-        top = "0"
-        left = "0"
-        zIndex = "1"
+      >
+        <Box 
+          onClick = {() => navigate("/")}
+          sx = {{ '&hover': { cursor: "pointer "}}}
+          color = {shades.secondary[500]}
         >
+          Beat Store
         </Box>
-    );
+        <Box
+          display = "flex"
+          justifyContent = "space-between"
+          columnGap = "20px"
+          zIndex = "2"
+        >
+          <IconButton sx={{ color: "black"}}>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton sx={{ color: "black"}}>
+            <PersonOutlined />
+          </IconButton>
+          <IconButton sx={{ color: "black"}}>
+            <ShoppingBagOutlined />
+          </IconButton>
+          <IconButton sx={{ color: "black"}}>
+            <MenuOutlined />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Navbar;
