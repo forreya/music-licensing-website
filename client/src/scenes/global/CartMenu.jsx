736127ff9f1b22675519132ea4d878084f0211b2
Ryan Lai <ryanlai12@gmail.com>
2023-01-04 17:26:06 +0000

    Building CartMenu. Created display to darken screen when cart open & created preliminary shopping cart

---
+++ client/src/scenes/global/CartMenu.jsx
@@ -0,0 +1,60 @@
+import { Box, Button, Divider, IconButton, Typography } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import styled from "@emotion/styled";
+import { shades } from "../../theme";
+import {
+  decreaseCount,
+  increaseCount,
+  removeFromCart,
+  setIsCartOpen,
+} from "../../state"
+import { useNavigate } from 'react-router-dom'
+
+// A styled 'component' that is used to reuse CSS code. It can be reused in our JSX code.
+const FlexBox = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const CartMenu = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+  const isCartOpen = useSelector((state) => state.cart.isCartOpen);
+
+  const totalPrice = cart.reduce((total, item) => {
+    return total + item.count * item.attributes.price;
+  }, 0);
+
+  return (
+    <Box
+      display = { isCartOpen? 'block' : 'none' }
+      backgroundColor = "rgba(0,0,0,0.4)"
+      position = "fixed"
+      zIndex = {10}
+      width = "100%"
+      height = "100%"
+      left = "0"
+      right = "0"
+      overflow = "auto"
+      >
+        <Box padding="30px" overflow="auto" height="100%">
+          {/* HEADER */}
+          <FlexBox mb="15px">
+            <Typography variant="h3">SHOPPING BAG ({cart.length})</Typography>
+            <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
+              <CloseIcon />
+            </IconButton>
+          </FlexBox>
+
+          {/* CART LIST */}
+        </Box>
+    </Box>
+  )
+}
+
+export default CartMenu;
