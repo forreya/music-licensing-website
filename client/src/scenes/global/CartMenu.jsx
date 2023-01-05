4a575e67620e905534bd7ab7b3612ccffcdd4290
Ryan Lai <ryanlai12@gmail.com>
2023-01-05 19:34:07 +0000

    Finished CSS / HTML for every beat in the shopping cart. Each beat will be rendered according to the map method on line 56 in CartMenu.jsx

--- client/src/scenes/global/CartMenu.jsx
+++ client/src/scenes/global/CartMenu.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, IconButton, Typography } from "@mui/material";
+import { Box, Button, Divider, Icon, IconButton, Typography } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import CloseIcon from "@mui/icons-material/Close";
 import AddIcon from "@mui/icons-material/Add";
@@ -26,8 +26,8 @@ const CartMenu = () => {
   const cart = useSelector((state) => state.cart.cart);
   const isCartOpen = useSelector((state) => state.cart.isCartOpen);
 
-  const totalPrice = cart.reduce((total, item) => {
-    return total + item.count * item.attributes.price;
+  const totalPrice = cart.reduce((total, beat) => {
+    return total + beat.count * beat.attributes.price;
   }, 0);
 
   return (
@@ -52,7 +52,59 @@ const CartMenu = () => {
           </FlexBox>
 
           {/* CART LIST */}
-        </Box>
+          <Box>
+            {cart.map((beat) => (
+              <Box key={`${beat.attributes.name}-${beat.id}`}>
+                <FlexBox p="15px 0">
+                  <Box flex="1 1 40%">
+                    <img 
+                      alt={beat?.name}
+                      width="123px"
+                      height="164px"
+                      src={`http://localhost:1337${beat?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+                    />
+                  </Box>
+                  <Box flex="1 1 60%">
+                    {/* ITEM NAME */}
+                    <FlexBox mb="5px">
+                      <Typography fontWeight="bold">
+                        {beat.attributes.name}
+                      </Typography>
+                      <IconButton onClick={() => dispatch(removeFromCart({ id: item.id}))}>
+                        <CloseIcon />
+                      </IconButton>
+                    </FlexBox>
+                    <Typography>{item.atrributes.shortDescription}</Typography>
+                    
+                    {/* AMOUNT */}
+                    <FlexBox m="15px 0">
+                      <Box
+                      display="flex"
+                      alignItems="center"
+                      border={`1.5px solid ${shades.neutral[500]}`}
+                      >
+                        <IconButton
+                          onClick={() => dispatch(decreaseCount({id:item.id}))}
+                        >
+                          <RemoveIcon />
+                        </IconButton>
+                        <Typography>{beat.count}</Typography>
+                        <IconButton
+                          onClick={() => dispatch(increaseCount({id:item.id}))}
+                        >
+                          <AddIcon />
+                        </IconButton>
+                      </Box>
+                    </FlexBox>
+                    {/* PRICE */}
+                    <Typography fontWeight="bold">${beat.attributes.price}</Typography>
+                  </Box>
+                </FlexBox>
+                <Divider />
+              </Box>
+            ))}
+          </Box>
+        </Box> 
     </Box>
   )
 }
