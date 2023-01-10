7407d9d8aa9f90f6338ed5856af30f6c8ba6e5a6
Ryan Lai <ryanlai12@gmail.com>
2023-01-10 19:18:26 +0000

    Created Beat.jsx which represents the reused component for each beat. Also set up 'MainCarousel' on the home page

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,6 +1,11 @@
+import MainCarousel from "./MainCarousel";
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className="home">
+            <MainCarousel />
+        </div>
+    )
 };
 
 export default Home;
